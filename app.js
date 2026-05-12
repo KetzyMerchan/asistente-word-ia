84a5deb41d4ab4c0081ad1bac9dfd4c922ac74d0
ketzy <ketzy26m@gmail.com>
2026-05-12 14:27:51 -0400

    actualizacion de estilos del boton nueva consulta

--- app.js
+++ app.js
@@ -102,8 +102,10 @@ function updateSelectedTextDisplay(text) {
 
 function showResult(msg, type) {
     var resultDiv = document.getElementById("result");
+
     var bg = "#e8f0fe";
     var border = "#667eea";
+
     if (type === "success") {
         bg = "#e6f4ea";
         border = "#34a853";
@@ -111,14 +113,18 @@ function showResult(msg, type) {
         bg = "#fce8e6";
         border = "#ea4335";
     }
+
     // Limpiar markdown basico (negritas, etc.)
     var mensajeLimpio = msg.replace(/\*\*/g, "").replace(/\*/g, "");
+    
     resultDiv.innerHTML =
-    "<div class=\"result-box\" style=\"background:" + bg + "; border-left-color:" + border + ";\">" +
-    mensajeLimpio.replace(/\n/g, "<br>") +
-    "<br><br>" +
-    "<button onclick='nuevaConsulta()' class='new-query-btn'>Nueva consulta</button>" +
-    "</div>";
+    "<div class='result-box' style='background:" + bg + "; border-left-color:" + border + ";'>" +
+        mensajeLimpio.replace(/\n/g, "<br>") +
+    "</div>" +
+
+    "<button onclick='nuevaConsulta()' class='new-query-btn'>" +
+        "Nueva consulta" +
+    "</button>";
 }
 
 function clearResult() {
